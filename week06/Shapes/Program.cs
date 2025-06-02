b76c0aed8cfe6ff53844a0c417d8c7db27fc0693
Daniel <cha24074@byui.edu>
2025-06-01 16:25:19 -0800

    practice for week06 done

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s = new Square("Orange", 5);
+        shapes.Add(s);
+
+        Rectangle r = new Rectangle("Red", 10, 5);
+        shapes.Add(r);
+
+        Circle c = new Circle("Blue", 3);
+        shapes.Add(c);
+
+        foreach (Shape t in shapes)
+        {
+            Console.WriteLine($"{t.GetColor()}, Area {t.CalculateArea()}");
+        }
+
     }
 }
